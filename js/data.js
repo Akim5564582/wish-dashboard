4a4b086ba822c3778f8c96a609b211fe2af522b0
Akim5564582 <kamusia1605@gmail.com>
2024-06-24 16:13:25 +0200

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Акім",
+  surname: "Северін",
+  userPhoto: "Флшь.png",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "Мерс.jpg", alt: "Мерс моєї мрії" },
+    { src: "Геймінг.jpg", alt: "Провесіональний геймінг" },
+    { src: "03.png", alt: "Скейтинг" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  200,
+  400,
+  500,
+  1888,
+  1200,
+  1500,
+  1800,
+  2100,
+  2400,
+  2700,
+  3000,
+  3300,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
